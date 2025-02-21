923ab145d851c1fff586df55e9adaee05fb9299b
Nataabuladze-collab <nata.abuladze1@icloud.com>
2025-02-21 20:38:09 +0400

    second commit

---
+++ day 04/homework/hw.py
@@ -0,0 +1,14 @@
+mother = "natia"
+
+father = "sulxani"
+
+sister_1 = "nia"
+
+sister_2= "mila"
+
+brother = "არ მყავს"
+
+print("i am " + me + " this is my family " + " my mother " + mother +  "," + "father "  + father + "," " sisters " + sister_1 + " and " + sister_2)
+
+
+
---
+++ day 04/homework/hw1.py
@@ -0,0 +1,28 @@
+#მიმატება
+print(245 + 12)
+print(123 + 234)
+print(12 + 45)
+print(56 + 789)
+print(87 + 3)
+
+#გამოკლება
+print(346 - 123)
+print(34 - 12)
+print(567 - 398)
+print(12 - 8)
+print(90 - 87)
+
+#გამრავლება
+print(56 * 12)
+print(13 * 3)
+print(67 * 2)
+print(5 * 9)
+print(78 * 12)
+
+#გაყოფა
+print(88 / 2)
+print(64 / 8)
+print(30 / 15)
+print(80 / 10)
+print(420 / 4)
+

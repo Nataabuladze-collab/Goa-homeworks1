3ff74b2cfa5cf7182dc9c5540f07c6ac8b4db2e1
Nataabuladze-collab <nata.abuladze1@icloud.com>
2025-03-12 18:54:41 +0400

    second commit

---
+++ day10/homework/hw1.py
@@ -0,0 +1,21 @@
+"""თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი"""
+#string ცვლადები
+name = "nata"
+surname = "abuladze"
+hobby = "dancer"
+location = "xulo"
+value = "nia"
+
+#integer ცვლადები
+age = 15
+number = 30
+number1 = 20
+number2 = 15
+number3 = 25
+
+#float ცვლადები
+height = 1.72
+wona = 50.0
+number4= 32.8
+number5 = 98.9
+number6 = 225.9
